d2d3f4b2cf8f2f528cafe3ea8c7ba641c0a354b6
Coco001 <wsgkk000@163.com>
2016-11-27 13:51:49 +0800

    4.3

--- src/Ex4_3.java
+++ src/Ex4_3.java
@@ -1,26 +1,25 @@
 /**
-对于一个元素各不相同且按升序排列的有序序列，请编写一个算法，创建一棵高度最小的二叉查找树。
-给定一个有序序列int[] vals,请返回创建的二叉查找树的高度。
-*/
+ * Created by Administrator on 2016/11/27.
+ */
+public class Ex4_3 {
 
-	/**
-	 * 高度最小的BST
+     /** 高度最小的BST
 	 * @param vals
 	 * @return
-	 */
-public class EX4_3{
-	private static int buildMinimalBST(int[] vals){
-//		return (int)Math.ceil(Math.log10(vals.length)/Math.log10(2));
-		
-		if (vals.length < 1)
-			return 0;
-		int height = 1;
-		int size = vals.length;
-		while (size > 1) {
-			height++;
-			size >>= 1;
-		}
-		return height;
-	}
-	
+      */
+    private static int buildMinimalBST(int[] vals){
+	//return (int)Math.ceil(Math.log10(vals.length)/Math.log10(2));
+
+        if (vals.length < 1)
+            return 0;
+        int height = 1;
+        int size = vals.length;
+        while (size > 1) {
+            height++;
+            size >>= 1;
+        }
+
+        return height;
+    }
+
 }
