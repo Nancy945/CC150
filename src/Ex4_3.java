83bffd75c979071a41ce243a75da1914309aa47e
Coco001 <wsgkk000@163.com>
2016-12-04 09:31:31 +0800

    4_3

--- src/Ex4_3.java
+++ src/Ex4_3.java
@@ -1,15 +1,11 @@
-/**
- * Created by Administrator on 2016/11/27.
- */
-public class Ex4_3 {
 
+public class Ex4_3 {
      /** 高度最小的BST
 	 * @param vals
 	 * @return
       */
-    private static int buildMinimalBST(int[] vals){
+    private int buildMinimalBST(int[] vals){
 	//return (int)Math.ceil(Math.log10(vals.length)/Math.log10(2));
-
         if (vals.length < 1)
             return 0;
         int height = 1;
@@ -18,9 +14,6 @@ public class Ex4_3 {
             height++;
             size >>= 1;
         }
-
-
         return height;
     }
-
 }
