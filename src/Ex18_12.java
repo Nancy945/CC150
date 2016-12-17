7fb8ef26b9d2d077dde9af3e4bbcaa8559a0e5be
Nancy <Nancy9452@gmail.com>
2016-12-17 21:37:41 +0800

    18.12

---
+++ src/Ex18_12.java
@@ -0,0 +1,14 @@
+/**
+ * 有一个正整数和负整数组成的NxN矩阵，请编写代码找出元素总和最大的子矩阵。请尝试使用一个高效算法。
+ * 给定一个int矩阵mat和矩阵的阶数n，请返回元素总和最大的子矩阵的元素之和。保证元素绝对值小于等于100000，且矩阵阶数小于等于200。
+ * 测试样例：
+ * [[1,2,-3],[3,4,-5],[-5,-6,-7]],3
+ * 返回：10
+ * Created by Nancy on 2016/12/17.
+ * Url:https://github.com/Nancy945/CC150
+ */
+public class Ex18_12 {
+    public int sumOfSubMatrix(int[][] mat, int n) {
+        // write code here
+    }
+}
