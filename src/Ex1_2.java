9465511b017a21b96e6524d3ae106de0f9d004ce
Nancy <Nancy9452@gmail.com>
2017-01-03 20:05:51 +0800

    18.12

--- src/Ex1_2.java
+++ src/Ex1_2.java
@@ -1,11 +1,11 @@
 
 /**
- 请实现一个算法，在不使用额外数据结构和储存空间的情况下，翻转一个给定的字符串(可以使用单个过程变量)。
- 给定一个string iniString，请返回一个string，为翻转后的字符串。保证字符串的长度小于等于5000。
- 测试样例：
- "This is nowcoder"
- 返回："redocwon si sihT"
-*/
+ * 请实现一个算法，在不使用额外数据结构和储存空间的情况下，翻转一个给定的字符串(可以使用单个过程变量)。
+ * 给定一个string iniString，请返回一个string，为翻转后的字符串。保证字符串的长度小于等于5000。
+ * 测试样例：
+ * "This is nowcoder"
+ * 返回："redocwon si sihT"
+ */
 public class Ex1_2 {
 
     //方法1
@@ -21,5 +21,23 @@ public class Ex1_2 {
     private static String reverseString(String iniString) {
         StringBuilder sb = new StringBuilder();
         return sb.append(iniString).reverse() + "";
+    }
+
+    //方法3
+    private static String reverseString1(String iniString) {
+        int head = 0;
+        int tail = iniString.length() - 1;
+        char[] chars = new char[iniString.length()];
+
+        while (head <= tail) {
+            char temp = iniString.charAt(head);
+            chars[head++] = iniString.charAt(tail);
+            chars[tail--] = temp;
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString1("abcdefg"));
     }
 }
--- src/Ex1_3.java
+++ src/Ex1_3.java
@@ -1,39 +1,57 @@
 
 /**
- *
  * @author 余屌丝 题目描述
- *
+ *         <p>
  *         给定两个字符串，请编写程序，确定其中一个字符串的字符重新排列后， 能否变成另一个字符串。这里规定大小写为不同字符，且考虑字符串重点空格。
  *         给定一个string stringA和一个string
  *         stringB，请返回一个bool，代表两串是否重新排列后可相同。保证两串的长度都小于等于5000。
- *
  */
 public class Ex1_3 {
 
-	private static int f;
+    private static int f;
 
-	public static boolean checkSam(String stringA, String stringB) {
-        if(stringA.length() != stringB.length())
-        	return false;
-        
-        for(int i = 0 ; i < stringA.length() ;i++){
-        	String c = stringA.charAt(i) +"";
-        	if(!stringB.contains(c)){
-        		return false;
-        	}else{
-        		stringB.replace(c,"");
-				f = 0;
-			}
+    public static boolean checkSam(String stringA, String stringB) {
+        if (stringA.length() != stringB.length())
+            return false;
+
+        for (int i = 0; i < stringA.length(); i++) {
+            String c = stringA.charAt(i) + "";
+            if (!stringB.contains(c)) {
+                return false;
+            } else {
+                stringB.replace(c, "");
+                f = 0;
+            }
         }
-		
-		return true;
-       
-	}
 
-	public static void main(String[] args) {
-		String A = "Here you are";
-		String B = "Are you here";
-		System.out.println(checkSam(A,B));
-	}
+        return true;
+
+    }
+
+    public static boolean checkSam1(String stringA, String stringB) {
+        if (stringA.length() != stringB.length()) {
+            return false;
+        }
+
+        int[] chars = new int[256];
+
+        for (int i = 0; i < stringA.length(); i++) {
+            char aChar = stringA.charAt(i);
+            char bChar = stringB.charAt(i);
+            chars[aChar]++;
+            chars[bChar]--;
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String A = "Here you are";
+        String B = "Are you here";
+        System.out.println(checkSam(A, B));
+    }
 
 }
